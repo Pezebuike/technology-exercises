583158f82e1dbe46e4182aab7cd57915a6ebfc02
cloudnloud <info@cloudnloud.com>
2022-03-01 01:34:56 +0100

    cicd

--- exercises/cicd/solutions/remove_builds_solution.groovy
+++ exercises/cicd/solutions/remove_builds_solution.groovy
@@ -1,1 +1,16 @@
-sfs
+def removeOldBuilds(buildDirectory, days = 14) {
+
+  def wp = new File("${buildDirectory}")
+  def currentTime = new Date()
+  def backTime = currentTime - days
+
+  wp.list().each { fileName ->
+      folder = new File("${buildDirectory}/${fileName}")
+      if (folder.isDirectory()) {
+          def timeStamp = new Date(folder.lastModified())
+          if (timeStamp.before(backTime)) {
+            folder.delete()
+          }
+      }
+  }
+}
